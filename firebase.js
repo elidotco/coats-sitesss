5adc60a0af6c689e4ae3b4d72d4004b552de23e4
Ebenezer Ametepeh <79024328+elidotco@users.noreply.github.com>
2023-10-07 12:27:19 +0000

    done

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgo80cDtYDR3xqqrgI_bZBQG4lMijF1Jo",
+  authDomain: "sitesss.firebaseapp.com",
+  projectId: "sitesss",
+  storageBucket: "sitesss.appspot.com",
+  messagingSenderId: "836999959892",
+  appId: "1:836999959892:web:d67220d02d0dd1324aa4c1",
+};
+export const app = initializeApp(firebaseConfig);
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+// import "../styles/dac.css";
+import "../styles/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.css";
+import "../styles/commun.css";
+import "../styles/mire.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
